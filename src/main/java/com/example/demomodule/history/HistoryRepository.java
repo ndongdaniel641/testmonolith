6e36fd72c3af05e0c5ead5b78a21183a34eb3bc8
uni2grow <uni2grow@uni2grow.uni2grow>
2025-10-24 14:43:12 +0100

    change

--- src/main/java/com/example/demomodule/history/HistoryRepository.java
+++ src/main/java/com/example/demomodule/history/HistoryRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long> {
     History findByTitle(String title);
+    History findByModuleAndIdentifier(String module, String identifier);
 }
